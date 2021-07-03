90e72991c75f41e07e3eb7227871271d08147c50
OmelianLevkovych <oml@e-boks.dk>
2021-07-03 09:58:22 +0300

    (jokes-telling) Added the jokes telling program. Working with strings and print() function

---
+++ src/3.JokesTelling/jokes.py
@@ -0,0 +1,20 @@
+#Jokes telling game
+#Working with strings and chars
+print('What do you get when you cross a snowman with a vampire?')
+input()
+print('Frostbite!')
+print()
+
+print("What do dentists call an astronaut's cavity?")
+input()
+print('A black hole!/n')
+print()
+
+print('Knock knock')
+input()
+print("Who's there?")
+input()
+print('Interrupting cow.')
+input()
+print('Interrupting cow wh', end='')
+print('-MOO!')
